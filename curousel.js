165bc4231588a0a7bbc39db333b019a720f74530
Jabin <15671592328@163.com>
2017-08-31 16:28:58 +0800

    上传源码到github-增加ES6的改动

--- curousel.js
+++ curousel.js
@@ -1,31 +1,40 @@
+/*
+*轮播新手教程，参考博主http://www.cnblogs.com/LIUYANZUO/p/5679753.html
+*删去了原代码中部分冗余的代码，简化逻辑，同时将代码改为ES6
+*采用js原生写就，适合夯实基础，但是，这段代码是毒，极难维护和扩展。
+*建议学习此代码了解思路之后看大神写的插件
+*/
 window.onload = function () {
-	var list = document.getElementById( 'list' );
-	var perv = document.getElementById( 'prev' );
-	var next = document.getElementById( 'next' );
+	let list = document.getElementById( 'list' );
+	let perv = document.getElementById( 'prev' );
+	let next = document.getElementById( 'next' );
 	
-	var buttons = document.getElementById( 'buttons' ).getElementsByTagName( 'span' );
+	let buttons = document.getElementById( 'buttons' ).getElementsByTagName( 'span' );
 	
-	var index = 1;
-	function buttonsShow() {
-		for( var i = 0; i < buttons.length; i++) {
+	let index = 1;
+	buttons[0].className = 'on';
+	list.style.left = 0;
+	
+	var buttonsShow = () => {
+		for( let i = 0; i < buttons.length; i++) {
 			buttons[i].className = '';
 		}
 		buttons[index-1].className = 'on';
 	}
-	function animation( offset ) {
-		var newLeft = parseInt(list.style.left) + offset;
+
+	var animation = ( offset ) => {
+		let newLeft = parseInt(list.style.left) + offset;
 		
 		list.style.left = newLeft + 'px';
-		if(newLeft<-3000){
-            list.style.left = -600 + 'px';
+		if( newLeft < -2400 ) {
+            list.style.left = 0 + 'px';
         }
-        if(newLeft>-600){
-            list.style.left = -3000 + 'px';
-        }
-		
+        if( newLeft > 0 ){
+            list.style.left = -2400 + 'px';
+        }	
 	}
 	
-	prev.onclick = function () {
+	prev.onclick = () => {
 		index -= 1;
 		if(index < 1){
 			index = 5;
@@ -34,38 +43,36 @@ window.onload = function () {
 		animation(600);
 	}
 	
-	next.onclick = function () {
+	next.onclick = () => {
 		index += 1;
-		if(index > 5){
+		if( index > 5 ){
 			index = 1;
 		}
 		buttonsShow();
-		animation(-600);
+		animation( -600 );
 	}
 	
-	var timer;
-	function play() {
-		timer = setInterval( function (){
+	let timer;
+	var play = () => {
+		timer = setInterval( () => {
 			next.onclick();
 			} , 3000 );
 	}
 	play();
-	var container = document.getElementById( 'container' );
-	function stop() {
+	let container = document.getElementById( 'container' );
+	var stop = () => {
 		clearInterval( timer );
 	}
 	container.onmouseover = stop;
 	container.onmouseout = play;
 	
-	for(var i = 0; i < buttons.length; i++ ){
-		(function(i) {
-		    buttons[i].onclick = function(){
-			    var clickIndex = parseInt(this.getAttribute('index'));
-			    var offset = 600 * (index - clickIndex);
+	for(let i = 0; i < buttons.length; i++ ){		
+		    buttons[i].onclick = () => {
+			    let clickIndex = parseInt(buttons[i].getAttribute('index'));
+			    let offset = 600 * (index - clickIndex);
 			    animation(offset);
 			    index = clickIndex;
 			    buttonsShow();
 		}
-		})(i);
 	}
 }
